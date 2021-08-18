33b8d1689b3b36f3a1a11c4cce6d36c2ef8acbc5
akhil-html <65633373+akhil-html@users.noreply.github.com>
2021-08-18 15:50:54 -0400

    first commit

---
+++ CP380-B2-BlockWebAPI-master/CP380-B2-BlockWebAPI/Controllers/BlocksController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
+
+namespace CP380_B2_BlockWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class BlocksController : ControllerBase
+    {
+        private readonly BlockList _blocklist;
+        public BlocksController(BlockList block)
+        {
+            _blocklist = block;
+        }
+
+        /// <summary>
+        /// Retrieve all the blocks in the chain
+        /// </summary>
+        /// <returns></returns>
+        [Route("/Blocks")]
+        [HttpGet]        
+        public IActionResult Get()
+        {
+            DateTime dt = DateTime.Now;
+            var objBlock = new Block(dt, null, new List<Payload>());
+            var result = new List<BlockSummary>()
+            {
+                new BlockSummary()
+                {
+                    Hash = objBlock.Hash,
+                    PreviousHash = objBlock.PreviousHash,
+                    TimeStamp = objBlock.TimeStamp
+                }
+            };
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Retrieve a specific block
+        /// </summary>
+        /// <returns></returns>
+        [Route("/Blocks/{hash}")]
+        [HttpGet]
+        public IActionResult GetHash(string hash)
+        {
+            DateTime dt = DateTime.Now;
+            var objBlock = new Block(dt, null, new List<Payload>());
+            objBlock.Hash = hash;
+            return Ok(objBlock);
+        }
+
+        /// <summary>
+        /// Retrieve all payloads for a given block
+        /// </summary>
+        /// <returns></returns>
+        [Route("/Blocks/{hash}/Payloads")]
+        [HttpGet]
+        public IActionResult GetPayload(string hash)
+        {
+            DateTime dt = DateTime.Now;
+            var objBlock = new Block(dt, null, new List<Payload>());
+            objBlock.Hash = hash;
+            return Ok(objBlock.Data);
+        }
+    }
+}
---
+++ CP380-B2-BlockWebAPI-master/CP380-B2-BlockWebAPI/Controllers/PendingPayloadsController.cs
@@ -0,0 +1,39 @@
+﻿using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CP380_B2_BlockWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PendingPayloadsController : ControllerBase
+    {
+        // TODO
+        [Route("/PendingPayloads")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Payload> data = new()
+            {
+                new Payload("user", TransactionTypes.GRANT, 10, null),
+                new Payload("user", TransactionTypes.BUY, 10, "10C"),
+            };
+            return Ok(data);
+        }
+
+        [Route("/PendingPayloads")]
+        [HttpPost]
+        public IActionResult Post(string user, TransactionTypes t, int amount, string item)
+        {
+            Payload ObjPayload = new(user, t, amount, item);
+            return Ok(ObjPayload);
+        }
+    }
+}
